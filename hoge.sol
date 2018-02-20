b737bc00239b17864809ac55d443005a40c4f532
Kouichi Umezawa <kouichiume@gmail.com>
2018-02-21 00:20:39 +0900

    first commit

---
+++ hoge.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.8;
+/** @title simple multiply . */
+contract Multiply7 {
+   event Print(uint);
+
+   /** @dev sample from solidity doc.
+      * @param input Width of the rectangle.
+      * @param h Height of the rectangle.
+      * @return s The calculated input * 7.
+      * @return p return the second argument .
+      */
+   function multiply(uint input,uint h) public returns (uint s, uint p) {
+      Print(input * 7);
+      return ( input * 7,h );
+   }
+}
